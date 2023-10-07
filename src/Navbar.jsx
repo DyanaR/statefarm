ecb1426eeca11e7de7cf0d19007d50b00e71897f
DyanaR <dyanarahhal98@gmail.com>
2023-10-07 13:41:33 -0500

    initial

---
+++ src/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from "styled-components";
+
+
+const Navbar = () => {
+  return (
+    <Container>
+        <div>
+            Navbar
+        </div>
+    </Container>
+    
+  )
+}
+
+export default Navbar
+
+const Container = styled.div`
+   
+    
+`;
